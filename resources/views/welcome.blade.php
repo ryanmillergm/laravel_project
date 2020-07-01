f69cc0ae864deeff3b66371422ea1d7e6f1f985b
Ryan Miller <ryanmillergm@gmail.com>
2020-07-01 08:58:37 -0600

    Updates welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ryan's Laravel Practice
                 </div>
 
                 <div class="links">
