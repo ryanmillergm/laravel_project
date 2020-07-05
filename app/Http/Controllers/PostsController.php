e9afe64b2bba3b412a22653df9fd1d12eaad386b
Ryan Miller <ryanmillergm@gmail.com>
2020-07-05 15:53:37 -0600

    Updates post controller to use post model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,26 +2,16 @@
 
 namespace App\Http\Controllers;
 
-class PostsController
+use App\Post;
+
+class PostsController 
 
 {
   public function show($slug)
   {
 
-    $post = \DB::table('posts')->where('slug', $slug)->first();
-
-    // dd($post);
-    // $posts = [
-    //     'my-first-post' => 'Hello, this is my first blog post!',
-    //     'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-    // ];
-
-    // if (! array_key_exists($post, $posts)) {
-    //     abort(404, 'Sorry, that post was not found.');
-    // }
-
     return view('post', [
-        'post' => $post
+      'post' => Post::where('slug', $slug)->firstOrFail()
     ]);
   }
 }
