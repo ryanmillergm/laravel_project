868848182a5ed16ddcbff4d6dea3a3f1bfa4cb71
Ryan Miller <ryanmiller@Ryans-MacBook-Pro.local>
2020-08-25 17:23:09 -0600

    Adds create view for articles

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layoutTemplate')
+
+@section('head')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"/>
+@endsection
+
+@section('content')
+    <div id="wrapper">
+        <div id="page" class="container">
+            <h1 class="heading has-text-weight-bold is-size-4">New Article</h1>
+
+            <form method="POST" action="/articles">
+                @csrf
+
+                <div class="field">
+                    <label class="label" for="title">Title</label>
+
+                    <div id="control">
+                        <input class="input" type="text" name="title" id="title">
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label" for="excerpt">Excerpt</label>
+
+                    <div id="control">
+                        <textarea class="textarea" name="excerpt" id="excerpt"></textarea>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label" for="body">Body</label>
+
+                    <div id="control">
+                        <textarea class="textarea" name="body" id="body"></textarea>
+                    </div>
+                </div>
+
+                <div class="field is-grouped">
+                    <div id="control">
+                       <button class="button is-link" type="submit">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
