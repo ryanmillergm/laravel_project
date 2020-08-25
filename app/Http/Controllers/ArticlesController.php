3a9367be76af72b3fd54553f0c48204c611b2a82
Ryan Miller <ryanmiller@Ryans-MacBook-Pro.local>
2020-08-25 17:22:21 -0600

    Adds create, store and edit

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,17 +7,59 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    public function index() 
+    public function index()
     {
         $articles = Article::latest()->get();
 
         return view('articles.index', ['articles' => $articles]);
     }
 
-    public function show($id) 
+    public function show($id)
     {
         $article = Article::find($id);
 
         return view('articles.show', ['article' => $article]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store()
+    {
+        // Responsible for persisting the new Article
+        // dump(request()->all());
+        $article = new Article();
+
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+
+        $article->save();
+
+        return redirect('/articles');
+    }
+
+    public function edit($id)
+    {
+        $article = Article::find($id);
+
+        // return view('articles.edit', ['article' => $article]);
+        // PHP compact function does the same as above
+        return view('articles.edit', compact('article'));
+    }
+
+    public function update($id)
+    {
+        // $article = Article::find($id);
+
+        // return view('article.show', ['article' => $article]);
+    }
+
+    public function destroy($id)
+    {
+        // $article = Article::find($id);
+
     }
 }
