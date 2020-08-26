016c0db5898c6e3f3c3d587b5e321c593ad76796
Ryan Miller <ryanmiller@Ryans-MacBook-Pro.local>
2020-08-25 18:16:37 -0600

    Adds validations to create and update

--- routes/web.php
+++ routes/web.php
@@ -71,4 +71,4 @@ Route::post('/articles', 'ArticlesController@store');
 Route::get('/articles/create', 'ArticlesController@create');
 Route::get('/articles/{article}', 'ArticlesController@show');
 Route::get('/articles/{article}/edit', 'ArticlesController@edit');
-// Route::put('/articles/{article}', 'ArticlesController@update');
+Route::put('/articles/{article}', 'ArticlesController@update');
